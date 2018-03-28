54c547767de662dacc8415d4c6e2f9e6953b8e74
calho <clho@ualberta.ca>
2018-03-28 16:05:08 -0600

    added comments to basic

--- basic.c
+++ basic.c
@@ -64,7 +64,6 @@ int main (int argc, char* argv[]){
 
     if (get_node_stat(&nodecount, &num_in_links, &num_out_links)) return 254;
 
-    // Calculate the result
     if (node_init(&nodehead, num_in_links, num_out_links, 0, nodecount)) return 254;
     
     r = malloc(nodecount * sizeof(double));
@@ -77,6 +76,7 @@ int main (int argc, char* argv[]){
     sendcounts = malloc(sizeof(int)*npes);
     displs = malloc(sizeof(int)*npes);
 
+    //this figures out the parition size that each process gets and its offset from the beginning of the array
     for (i = 0; i < npes; i++) {
         sendcounts[i] = nodecount/npes;
         if (rem > 0) {
@@ -95,38 +95,26 @@ int main (int argc, char* argv[]){
         r[i] = 1.0 / nodecount;
     damp_const = (1.0 - DAMPING_FACTOR) / nodecount;
 
-    // if (myrank == 0)
-    // {   
-    //     char processor_name[MPI_MAX_PROCESSOR_NAME];
-    //     int name_len;
-    //     MPI_Get_processor_name(processor_name, &name_len);
-    //     printf("processor %s started\n",  processor_name);
-    // }
-
    
     GET_TIME(start);
     // CORE CALCULATION
     do{
         ++iterationcount;
 
-
-       
-        // printf("pre from %i:\n", myrank );
-        // printArray(r_pre, nodecount);
+        //copy r to r_pre
         vec_cp(r, r_pre, nodecount); 
-
-        // MPI_Scatter(r, local_nodecount, MPI_DOUBLE, local_r, local_nodecount, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        
+        //master process tasks
         if (myrank == 0){
             // printf("%d started\n", myrank );
             for (dest=1; dest<npes; dest++) {
-                // MPI_Send(&r_pre[0], nodecount, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
-                // printf("%d sent r_pre to %d\n", myrank, dest );
-                // MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+               
+                //send to each process its parition of the array
                 MPI_Send(&r[displs[dest]], sendcounts[dest], MPI_DOUBLE, dest, 2, MPI_COMM_WORLD);
-                // printf("%d sent r[%d] to %d\n", myrank, offset, dest );
-                // printf("Sent %d elements to task %d offset= %d\n",chunksize,dest,offset);
+
             }
 
+            //do the page rank algorithm
             for ( i = 0; i < local_nodecount; ++i){
                 r[i] = 0;
                 for ( j = 0; j < nodehead[i].num_in_links; ++j)
@@ -135,46 +123,33 @@ int main (int argc, char* argv[]){
                 r[i] += damp_const;
             }
 
+            //grab the partitions that each process worked on and build up the master r 
             for (dest=1; dest<npes; dest++) {
                 MPI_Recv(&r[displs[dest]], sendcounts[dest], MPI_DOUBLE, dest, 3, MPI_COMM_WORLD, &status);
-                // printf("%d recv r from %d\n", myrank, dest );
             }
         }
+
+        //all other processes
         if(myrank > 0){
-            // printf("%d started\n", myrank);
-            // MPI_Recv(&r_pre[0], nodecount, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
-            // printf("%d recv r_pre\n", myrank );
-            // MPI_Recv(&offset, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+            
+            //get the parition this process is assigned
             MPI_Recv(&r[offset], local_nodecount, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD, &status);
-            // printf("%d recv r[%d] = %d\n", myrank, offset,r[offset] );
-            // printArray(r, nodecount,0);
+
+            //do the page rank algorithm
             for ( i = offset; i < local_nodecount+offset; ++i){
-                // printf("%d starting at %d\n",myrank, offset );
                 r[i] = 0;
                 for ( j = 0; j < nodehead[i].num_in_links; ++j)
                     r[i] += r_pre[nodehead[i].inlinks[j]] / num_out_links[nodehead[i].inlinks[j]];
                 r[i] *= DAMPING_FACTOR;
                 r[i] += damp_const;
             }
-            // printf("r from %d\n", myrank );
-            // printArray(r, nodecount, 0);
 
+            //send the process' completed partition back to master
             MPI_Send(&r[offset], local_nodecount, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
-            // printf("%d sent r[%d]\n", myrank, local_nodecount*myrank );
         }
 
+        //broadcast the complete r array from master to all others
         MPI_Bcast(&r[0], nodecount, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-        // printf("local_r:\n");
-        // printArray(local_r, nodecount);
-
-
-        // MPI_Allgather(local_r, local_nodecount, MPI_DOUBLE, r, local_nodecount, MPI_DOUBLE, MPI_COMM_WORLD);
-        // if (myrank == 0)
-        // {   
-        //     printf("r:\n");
-        //     printArray(r, nodecount);
-        // }
 
     }while(rel_error(r, r_pre, nodecount) >= EPSILON);
     //printf("Program converges at %d th iteration.\n", iterationcount);
